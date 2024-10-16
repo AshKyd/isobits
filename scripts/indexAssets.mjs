0944423a2d178855ac79afaf7f203b42b90db8ea
Ash Kyd <ash@kyd.com.au>
2024-10-16 20:44:14 +1000

    feature: more structure to the index

--- scripts/indexAssets.mjs
+++ scripts/indexAssets.mjs
@@ -9,15 +9,17 @@ const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 function readDir(dir) {
   const items = {
-    files: [],
+    type: "folder",
+    name: path.basename(dir),
+    children: [],
   };
   const listing = fs.readdirSync(dir).filter((entry) => !entry.match(/^\./));
   listing.forEach((file) => {
     const fullPath = path.resolve(dir, file);
     if (fs.lstatSync(fullPath).isDirectory()) {
-      items[path.basename(file)] = readDir(fullPath);
+      items.children.push(readDir(fullPath));
     } else if (file.match(/\.svg$/)) {
-      items.files.push(path.basename(file));
+      items.children.push({ type: "file", name: path.basename(file) });
     }
   });
   return items;
